0a69b014467203306e26e1d0d599de5756ffd8cf
Vincenzo Caruso <enzo@carusovincenzo.com>
2022-09-27 16:46:38 +0200

    Initial Commit

---
+++ ManualWebContent/ManualWebContentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManualWebContentApp.swift
+//  ManualWebContent
+//
+//  Created by Vincenzo Caruso on 27/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ManualWebContentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
